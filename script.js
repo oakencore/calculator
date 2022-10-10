e619c96d4136498cbde868fc070279ee50432bea
oakencore <105373906+oakencore@users.noreply.github.com>
2022-10-10 13:53:52 +0200

    bugfix for equals function

--- script.js
+++ script.js
@@ -12,105 +12,161 @@ let selectedOperand = "";
 let previousOperand = "";
 let ongoingTotal = [];
 let operandCounter = 0;
+let decimalCounter = 0;
 
+// FUNCTIONS
+function numbers(number) {
+  displayValue += number;
+  displayPrimary.textContent = displayValue;
+}
+
+function decimal(decimal) {
+  if (decimalCounter >= 1) displayPrimary.textContent = displayValue;
+  else {
+    displayValue += decimal;
+    displayPrimary.textContent = displayValue;
+    decimalCounter++;
+  }
+}
+
+function operand(operand) {
+  if (operandCounter == 0 && ongoingTotal.length === 0) {
+    operandCounter++;
+    selectedOperand = buttonValue;
+    previousOperand = buttonValue;
+    firstNumber = parseFloat(displayValue);
+    displayValue = [];
+    displayPrimary.textContent = selectedOperand;
+    decimalCounter = 0;
+  } else {
+    if (ongoingTotal.length === 0) {
+      selectedOperand = buttonValue;
+      previousNumber = parseFloat(displayValue);
+      ongoingTotal = parseFloat(
+        operate(firstNumber, previousNumber, previousOperand)
+      );
+      displayPrimary.textContent = ongoingTotal;
+      displayValue = [];
+      previousOperand = buttonValue;
+      decimalCounter = 0;
+    } else if (ongoingTotal.length != 0) {
+      selectedOperand = buttonValue;
+      firstNumber = parseFloat(displayValue);
+      previousNumber = ongoingTotal;
+      ongoingTotal = parseFloat(
+        operate(previousNumber, firstNumber, previousOperand)
+      );
+      displayValue = [];
+      previousOperand = buttonValue;
+      displayPrimary.textContent = ongoingTotal;
+      decimalCounter = 0;
+    }
+  }
+}
+
+function allClear() {
+  displayValue = [];
+  selectedOperand = "";
+  previousOperand = "";
+  ongoingTotal = [];
+  operandCounter = 0;
+  firstNumber = 0;
+  previousNumber = 0;
+  decimalCounter = 0;
+  displayPrimary.textContent = displayValue;
+}
+
+function intermittentClear() {
+  displayValue = [];
+  selectedOperand = "";
+  previousOperand = "";
+  ongoingTotal = [];
+  operandCounter = 0;
+  firstNumber = 0;
+  decimalCounter = 0;
+}
+
+// Operate Switch function
+const operate = function (number1, number2, operator) {
+  switch (operator) {
+    case "+":
+      return parseFloat(number1) + parseFloat(number2);
+      break;
+    case "-":
+      return parseFloat(number1) - parseFloat(number2);
+      break;
+    case "*":
+      return parseFloat(number1) * parseFloat(number2);
+      break;
+    case "/":
+      return parseFloat(number1) / parseFloat(number2);
+      break;
+  }
+};
+
+function equals() {
+  if (operandCounter === 0) {
+    console.log("first statement hit");
+    displayPrimary.textContent = displayValue;
+  } else if (displayValue == 0 && selectedOperand == "/") {
+    console.log("second statement hit");
+    displayPrimary.textContent = "#DIV/0!";
+    //intermittentClear()
+  } else if (operandCounter == 0 || ongoingTotal.length === 0) {
+    console.log("third statement hit");
+    operateResult =
+      Math.round(
+        operate(firstNumber, displayValue, selectedOperand) * 10000000
+      ) / 10000000;
+    displayPrimary.textContent = operateResult;
+    //intermittentClear()
+  } else {
+    console.log("fourth statement hit");
+    firstNumber = displayValue;
+    previousNumber = ongoingTotal;
+    operateResult =
+      Math.round(
+        operate(previousNumber, firstNumber, selectedOperand) * 10000000
+      ) / 10000000;
+    displayPrimary.textContent = operateResult;
+    //intermittentClear()
+  }
+  intermittentClear();
+}
+
+//EVENT LISTENERS
 //Event listener for number buttons
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if ((buttonValue = button.getAttribute("data-num"))) {
-      displayValue += buttonValue;
-      displayPrimary.textContent = displayValue;
+      numbers(buttonValue);
     }
   });
 });
 
-// . button event listener
+// '.' button event listener
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if ((buttonValue = button.getAttribute("data-decimal"))) {
-      displayValue += buttonValue;
-      displayPrimary.textContent = displayValue;
+      decimal(buttonValue);
     }
   });
 });
-
-//Event listener for Data Operands
-//DONE: Chain multiple operations together 12 + 7 - 5 * 3 = 42
-//DONE: Handle single AND multiple operations.
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if ((buttonValue = button.getAttribute("data-op"))) {
-      // If no operators have been pressed, do this:
-      if (operandCounter == 0 && ongoingTotal.length === 0) {
-        operandCounter++;
-        selectedOperand = buttonValue;
-        previousOperand = buttonValue;
-        firstNumber = parseInt(displayValue);
-        displayValue = [];
-        displayPrimary.textContent = selectedOperand;
-        // If 1 operators have been pressed already, do this:
-      } else {
-        if (ongoingTotal.length === 0) {
-          selectedOperand = buttonValue;
-          previousNumber = parseInt(displayValue);
-          ongoingTotal = parseInt(
-            operate(firstNumber, previousNumber, previousOperand)
-          );
-          displayPrimary.textContent = ongoingTotal;
-          displayValue = [];
-          previousOperand = buttonValue;
-        } else if (ongoingTotal.length != 0) {
-          selectedOperand = buttonValue;
-          firstNumber = parseInt(displayValue);
-          previousNumber = ongoingTotal;
-          ongoingTotal = parseInt(
-            operate(previousNumber, firstNumber, previousOperand)
-          );
-          displayValue = [];
-          previousOperand = buttonValue;
-          displayPrimary.textContent = ongoingTotal;
-        }
-      }
-    }
-  });
-});
-
-
 
 // Equals button event listener
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.getAttribute("Data-Equals")) {
-      if (operandCounter === 0) {
-        displayPrimary.textContent = displayValue;
-      } else if (displayValue == 0 && selectedOperand == "/") {
-        displayPrimary.textContent = "You cannot divide by zero LMFAO";
-        displayValue = [];
-        selectedOperand = "";
-        previousOperand = "";
-        ongoingTotal = [];
-        operandCounter = 0;
-        firstNumber = 0;
-      } else if (operandCounter == 0 || ongoingTotal.length === 0) {
-        operateResult = operate(firstNumber, displayValue, selectedOperand);
-        displayPrimary.textContent = operateResult;
-        displayValue = [];
-        selectedOperand = "";
-        previousOperand = "";
-        ongoingTotal = [];
-        operandCounter = 0;
-        firstNumber = 0;
-      } else {
-        firstNumber = displayValue;
-        previousNumber = ongoingTotal;
-        operateResult = operate(previousNumber, firstNumber, selectedOperand);
-        displayPrimary.textContent = operateResult;
-        displayValue = [];
-        selectedOperand = "";
-        previousOperand = "";
-        ongoingTotal = [];
-        operandCounter = 0;
-        firstNumber = 0;
-      }
+      equals();
+    }
+  });
+});
+
+// OPERAND event listener
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if ((buttonValue = button.getAttribute("data-op"))) {
+      operand(buttonValue);
     }
   });
 });
@@ -119,33 +175,12 @@ buttons.forEach((button) => {
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.getAttribute("Data-AllClear")) {
-      displayValue = [];
-      selectedOperand = "";
-      previousOperand = "";
-      ongoingTotal = [];
-      operandCounter = 0;
-      firstNumber = 0;
-      previousNumber = 0;
-      displayPrimary.textContent = displayValue;
-      console.log("ALL_CLEAR");
+      allClear();
     }
   });
 });
 
-// Operate Switch function
-const operate = function (number1, number2, operator) {
-  switch (operator) {
-    case "+":
-      return parseInt(number1) + parseInt(number2);
-      break;
-    case "-":
-      return parseInt(number1) - parseInt(number2);
-      break;
-    case "*":
-      return parseInt(number1) * parseInt(number2);
-      break;
-    case "/":
-      return parseInt(number1) / parseInt(number2);
-      break;
-  }
-};
+//TODO: Keyboard support
+document.addEventListener("keypress", (event) => {
+  numbers(event.key);
+});
